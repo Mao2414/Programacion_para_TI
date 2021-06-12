6d8383371760102ab4c9a148be04fa9acc3e4c5e
Mao2414 <85120551+Mao2414@users.noreply.github.com>
2021-06-11 21:14:54 -0600

    Add files via upload

---
+++ Extraclases/Extraclase1.java
@@ -0,0 +1,93 @@
+public class Extraclase {
+	public double calcularimpuesto (int numdependientes, double salario) {
+		double resultado=0;
+		double montopordependiente;
+		
+		if (dependientes=1){
+			montoporependiente=salario*1;
+			if (salario<200000){
+				resultado=0;
+			}
+			else if (salario<400000){
+				resultado=montopordependiente*0.1;
+			}
+			else if (salario<600000){
+				resultado=montopordependiente*0.15;
+			}
+			else if (salario<800000){
+				resultado=montopordependiente*0.2;
+			
+			else{
+				resultado=montopordependiente*0,3
+			
+	else if (dependientes =2){
+		resultado=salario*0,9;
+	}if (salario<200000){
+				resultado=0;
+			}
+			else if (salario<400000){
+				resultado=montopordependiente*0.1;
+			}
+			else if (salario<600000){
+				resultado=montopordependiente*0.15;
+			}
+			else if (salario<800000){
+				resultado=montopordependiente*0.2;
+			
+			else{
+				resultado=montopordependiente*0,3
+	}
+	else if (dependientes =3){
+		resultado=salario*0,7;
+		if (salario<200000){
+				resultado=0;
+			}
+			else if (salario<400000){
+				resultado=montopordependiente*0.1;
+			}
+			else if (salario<600000){
+				resultado=montopordependiente*0.15;
+			}
+			else if (salario<800000){
+				resultado=montopordependiente*0.2;
+			
+			else{
+				resultado=montopordependiente*0,3
+	}	
+	else if (dependientes =4){
+		resultado=salario*0,6;
+		if (salario<200000){
+				resultado=0;
+			}
+			else if (salario<400000){
+				resultado=montopordependiente*0.1;
+			}
+			else if (salario<600000){
+				resultado=montopordependiente*0.15;
+			}
+			else if (salario<800000){
+				resultado=montopordependiente*0.2;
+			
+			else{
+				resultado=montopordependiente*0,3
+	}
+	else{
+		resultado=salario*0,4;
+		if (salario<200000){
+				resultado=0;
+			}
+			else if (salario<400000){
+				resultado=montopordependiente*0.1;
+			}
+			else if (salario<600000){
+				resultado=montopordependiente*0.15;
+			}
+			else if (salario<800000){
+				resultado=montopordependiente*0.2;
+			
+			else{
+				resultado=montopordependiente*0,3
+	}
+	return resultado;	
+}		
+
